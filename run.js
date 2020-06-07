1fc72ca257b5f100b5c7d6ad61073c7330af1a92
Peter Salzmann <petersalzmann@gmail.com>
2020-06-08 03:49:51 +0930

    removed the counter as was testing sending commands.

--- run.js
+++ run.js
@@ -10,7 +10,6 @@ var servers = require('./servers');
   var UDP_DEST = '255.255.255.255';
   var UDP_PORT = 4445;
   var BROADCAST_DELAY_MS = 4000;
-  var counter = 0.0;
 
   async.forever(
     function (next) {
@@ -18,19 +17,14 @@ var servers = require('./servers');
         var msg = Buffer.from(s.msg)
         if (msg) {
           if (udp_broadcaster) {
-            if (counter === 12){
             udp_broadcaster.send(msg, 0, msg.length, UDP_PORT, UDP_DEST);
-            counter+=1;
             } else { proc.stdin.write('stop') }
-          } else {
-
             udp_broadcaster = dgram.createSocket('udp4');
             udp_broadcaster.bind(UDP_PORT, s.ip);
             udp_broadcaster.on('listening', function () {
               udp_broadcaster.setBroadcast(true);
               udp_broadcaster.send(msg, 0, msg.length, UDP_PORT, UDP_DEST);
             });
-            counter = 1;
             udp_broadcaster.on("error", function (err) {
               logging.error("Cannot bind broadcaster");
             });
@@ -46,23 +40,10 @@ var proc = child_process.spawn('java.exe', ['-Xmx4096M', '-Xms4096M', '-jar', "C
 
 // proc.stdout.pipe(process.stdout);
 proc.on('error', err => {
-  logging.error(`error: ${err}`)
+  logging.error(`error: ${err}`);
+  process.exit( );
 });
 proc.on('close', code => {
   logging.debug(`exited with code ${code}`);
   process.exit();
-});
-
-// catching signals and do something before exit
-['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-  'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-].forEach(function (sig) {
-  process.on(sig, function () {
-    proc.stdin.write('stop');;
-    console.log('signal: ' + sig);
-  });
-});
-
-process.on('exit', function () {
-  proc.stdin.write('stop');
 });
